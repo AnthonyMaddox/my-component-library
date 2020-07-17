cb7e0992963dba782e5106537719e39a8a0519ab
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-07-17 12:37:47 -0400

    input fields done

---
+++ src/components/InputField/InputField.stories.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import InputField from "./InputField";
+import SelectField from "./SelectField";
+
+storiesOf("Input Fields", module)
+  //email
+  .add("Email", () => <InputField type="Email" />)
+  .add("Email Medium", () => <InputField medium type="Email" />)
+  .add("Email Large", () => <InputField large type="Email" />)
+
+  //select
+  .add("Select Outline", () => <SelectField outline type="select" label="Select" />)
+
+  .add("Select Outline Medium", () => <SelectField outline medium label="Select" type="select" />)
+
+  .add("Select Outline Large", () => <SelectField outline large label="Select" type="select" />)
+  .add("Select", () => <SelectField type="select" label="Select" />)
+
+  .add("Select Medium", () => <SelectField medium label="Select" type="select" />)
+
+  .add("Select Large", () => <SelectField large label="Select" type="select" />)
---
+++ src/components/InputField/SelectField.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./InputField.css";
+
+const SelectField = (props) => {
+  let classList = [];
+  let types = ["select"];
+  if (types.includes(props.type)) {
+    classList.push(`select-${props.type}`);
+  }
+  if (props.medium) {
+    classList.push(`select-${props.type}-medium`);
+  }
+  if (props.large) {
+    classList.push(`select-${props.type}-large`);
+  }
+  if (props.outline) {
+    classList.push(`select-${props.type}-outline`);
+  }
+
+  return (
+    <select className={classList.join(" ")}>
+      <option label={props.label}></option>
+    </select>
+  );
+};
+export default SelectField;
