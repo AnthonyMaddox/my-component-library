02b68e1e903573039562313fbf193f4db6fa8dca
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-07-20 19:50:34 -0400

    ran npm build storybook

---
+++ story-build/precache-manifest.8777cd7999d2e02e80eca70f79555f4c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fdd3e8e8c04a59fa060d442d66ce447",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.b346d0ff65d3a1eea029.bundle.js"
+  },
+  {
+    "url": "runtime~main.b346d0ff65d3a1eea029.bundle.js"
+  },
+  {
+    "revision": "52c8cc47bac6ace7be0a",
+    "url": "static/css/main.76aa3df5.chunk.css"
+  },
+  {
+    "revision": "176c42bc94b35aa3f42de84b7552ec40",
+    "url": "static/media/bose.176c42bc.png"
+  },
+  {
+    "revision": "8655819c918b35e17debde254bc9da15",
+    "url": "static/media/cart_2.8655819c.png"
+  },
+  {
+    "revision": "0311a4ad4a60229c7ce7860cca0c72c9",
+    "url": "static/media/cart_white.0311a4ad.png"
+  },
+  {
+    "revision": "c307014ca46b79ed267faad122a63820",
+    "url": "static/media/hearts.c307014c.png"
+  },
+  {
+    "revision": "36c7a74d00a26e3098a1ac9ffb066e00",
+    "url": "static/media/hearts_white.36c7a74d.png"
+  },
+  {
+    "revision": "c0a117bc543f65a8b50462b8767ee0f1",
+    "url": "static/media/macbook.c0a117bc.png"
+  },
+  {
+    "url": "vendors~main.b346d0ff65d3a1eea029.bundle.js"
+  },
+  {
+    "url": "vendors~main.b346d0ff65d3a1eea029.bundle.js.LICENSE.txt"
+  }
+]);
