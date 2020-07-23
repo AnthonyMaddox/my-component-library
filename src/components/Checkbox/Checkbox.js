55856711cb531df17509561118e3ee619c0de013
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-07-23 09:28:01 -0400

    working on checkbox functionality

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Checkbox.css";
 
-const Checkbox = (props) => {
-  let classList = [];
-  /*let types = ["Checkbox"];
+class Checkbox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      classList: [],
+      checked: true,
+    };
+
+    /*let types = ["Checkbox"];
   if (types.includes(props.type)) {
     classList.push(`checkbox-${props.type}`);
   */
-  
-  if (props.black) {
-    classList.push("innerBlackbox");
+
+    if (props.black) {
+      this.state.classList.push("innerBlackbox");
+    }
+    if (props.noDisplay) {
+      this.state.classList.push("noDisplay");
+    }
   }
-  if (props.noDisplay) {
-   classList.push("noDisplay");
- }
- if (props.blue) {
-   classList.push("innerBlueBox");
- }
-  return (
-    <div className="outerBlackBox">
-      <div className={classList.join(" ")}>&#x2713;</div>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="outerBlackBox">
+        <div className={this.state.classList.join(" ")}>&#x2713;</div>
+      </div>
+    );
+  }
+}
 
 export default Checkbox;
